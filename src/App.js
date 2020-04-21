cbdf67b0c4cf8574dbaa22a4270fee59017702e7
GHANEM <mohsenghanem02@gmail.com>
2020-04-21 15:06:48 +0200

    32 sauvgarder state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {sampleText} from './sampleText';
+import React, { Component } from "react";
+import "./App.css";
+import { sampleText } from "./sampleText";
+import marked from 'marked'
 
 class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  handleChange = (event) => {
+    const text = event.target.value;
+    this.setState({ text });
+  };
+
+  renderText = text => { 
+
+const __html = marked(text,{sanitize:true})
+    return {__html}
+  }
+
   render() {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-sm-6'>
-            <textarea value={sampleText} className='form-control' rows='35'>
-            </textarea>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea
+              onChange={this.handleChange}
+              value={this.state.text}
+              className="form-control"
+              rows="35"
+            ></textarea>
           </div>
-           <div className='col-sm-6'>  
-           <h1>  Résultat </h1>
-
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML= {this.renderText(this.state.text)} />
           </div>
         </div>
       </div>
