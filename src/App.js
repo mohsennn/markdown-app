257310c717208f017977db5c20bceaac2ae130f5
GHANEM <mohsenghanem02@gmail.com>
2020-04-21 15:38:27 +0200

    33 add localStoage , Lifecycle compoent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from "react";
 import "./App.css";
 import { sampleText } from "./sampleText";
-import marked from 'marked'
+import marked from "marked";
 
 class App extends Component {
   state = {
-    text: sampleText
-  }
+    text: sampleText,
+  };
 
   handleChange = (event) => {
     const text = event.target.value;
     this.setState({ text });
   };
 
-  renderText = text => { 
+  renderText = (text) => {
+    const __html = marked(text, { sanitize: true });
+    return { __html };
+  };
 
-const __html = marked(text,{sanitize:true})
-    return {__html}
+  componentDidMount() {
+    console.log('montéééé !!!');
+    const text = localStorage.getItem('text')
+    if (text) {
+      this.setState({ text });
+    } else {
+      this.setState({ text: sampleText });
+    }
   }
-
+  componentDidUpdate() {
+    const { text } = this.state
+    localStorage.setItem('text', text);
+  }
   render() {
     return (
       <div className="container">
@@ -32,7 +44,7 @@ const __html = marked(text,{sanitize:true})
             ></textarea>
           </div>
           <div className="col-sm-6">
-            <div dangerouslySetInnerHTML= {this.renderText(this.state.text)} />
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
           </div>
         </div>
       </div>
